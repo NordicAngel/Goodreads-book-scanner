83a2e5efd74cd26de863fc943490f0fbff9b6d73
Sebastian Gylstorff <seba124m@edu.zealand.dk>
2022-11-28 14:19:42 +0100

    created the base for a websocket

--- web side/index.js
+++ web side/index.js
@@ -6,3 +6,26 @@ const app = Vue.createApp({
         }
     },
 );
+
+function SetupWebSocket(){
+
+    let ip = "ws://10.200.178.183:12000"
+
+    var ws = new WebSocket(ip)
+
+    ws.onopen = function(){
+        alert("Connection is open")
+    }
+
+    ws.onmessage = function(evt){
+        console.log(`isbn is: ${evt.data}`)
+        
+    }
+
+    while (true){
+        console.log(ws.readyState)
+    }
+}
+
+SetupWebSocket()
+
