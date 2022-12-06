825037f1e105fb33f86ec4cb5de85834d6a60199
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-12-06 12:24:57 +0100

    model files

---
+++ RestfulOpenLibrary/OpenLibraryWebServiceLibrary/Model/List_Names.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulLibrary.Model
+{
+    public class List_Names
+    {
+        public int ID { get; set; }
+        public string List_Name { get; set; }
+
+        public List_Names()
+        {
+
+        }
+
+        public List_Names(int id, string listname)
+        {
+            ID = id;
+            List_Name = listname;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
